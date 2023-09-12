169bba51b3b4716fad065ac3cc4cca547b4323d5
David Velasquez Rojas <drvelasquezr@outlook.com>
2023-09-12 17:19:31 -0500

    change key vault parameters

--- Templates/Scripts/upload_to_blob.ps1
+++ Templates/Scripts/upload_to_blob.ps1
@@ -1,15 +1,16 @@
 param(
     [string]$resourceGroup,
-    [string]$containerName,
     [string]$storageAccountName,
     [string]$outputFileName,
+    [string]$storageContainerName,
     [string]$storageAccountKey
 )
 
 if (-not (Get-Module -Name Az.Storage -ListAvailable)) {
     Install-Module -Name Az.Storage -Force -AllowClobber
 }
-$filePath = "$resourceGroup_$outputFileName"
+
+$filePath = '$resourceGroup_$outputFileName'
 $blobName = '$containerName/$resourceGroup_$outputFileName'
 
 $storageContext = New-AzStorageContext -StorageAccountName $(storageAccountName) -StorageAccountKey $(storageAccountAccessKey)
