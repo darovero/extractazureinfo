0f66eef2621afd93f00a2f67c984f78e4453c1d3
David Velasquez Rojas <drvelasquezr@outlook.com>
2023-09-12 21:39:08 -0500

    change web sites parameters

---
+++ Templates/Scripts/web_site_info.ps1
@@ -0,0 +1,60 @@
+param(
+    [string]$resourceGroup,
+    [string]$outputFilePath
+)
+
+# Get Web Site in Resource Group
+$website = Get-AzResource -ResourceGroupName $resourcegroup -ResourceType Microsoft.Web/sites
+        
+# Initialize content variable
+$ExportContent = ""
+
+# Loop through each Web Site
+foreach ($web in $website) {
+    $webName = $web.Name
+
+    # Get Properties
+    $properties = Get-AzWebApp -ResourceGroupName $resourcegroup -Name $webName
+
+    # Tags
+    $tags = $properties.Tags
+
+    # SiteConfig
+    $siteconfig = $properties.SiteConfig
+
+    # AppSettings
+    $appsettings = $properties.SiteConfig.AppSettings
+
+    # VirtualApplications
+    $virtualapplications = $properties.SiteConfig.VirtualApplications
+
+    # Cors
+    $cors = $properties.SiteConfig.Cors 
+
+    # Append information to ExportContent
+    $ExportContent += @"
+Key Vault: $vaultName
+
+Properties:
+$($Properties | Select-Object -ExcludeProperty Tags,SiteConfig | Format-List | Out-String -Width 4096)
+
+Tags:
+$($tags | Format-List | Out-String -Width 4096)
+       
+SiteConfig:
+$($SiteConfig | Select-Object -ExcludeProperty DefaultDocuments,AppSettings,VirtualApplications,Experiments,Cors | Format-List | Out-String -Width 4096)
+
+AppSettings:
+$($appsettings | Format-List | Out-String -Width 4096)
+
+VirtualApplications:
+$($virtualapplications | Format-List | Out-String -Width 4096)
+
+Cors:
+$($Cors | Format-List | Out-String -Width 4096)
+
+"@
+}
+
+# Export the information to a text file
+$ExportContent | Out-File -FilePath $outputFilePath -Encoding UTF8
